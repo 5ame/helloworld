6964e9903cf7782f0fa531d898b51b6abd752b3e
5ame <100693377@unimail.derby.ac.uk>
2024-10-01 15:11:08 +0100

    work

---
+++ 1.2.py
@@ -0,0 +1,5 @@
+print("Single line comments start with #")
+
+# print("Hello")
+# print("World")
+# print("with Comments")
---
+++ 1.3.py
@@ -0,0 +1,4 @@
+print(type(12+5))
+print(type(5+3.0))
+print(type(12/5))
+print(type(12+5==5*3.0))

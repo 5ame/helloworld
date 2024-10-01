edf1ffbe8200cca2c14aca1c2a0465fb0c9ebcc8
5ame <100693377@unimail.derby.ac.uk>
2024-10-01 15:48:35 +0100

    .

---
+++ task 2/2.3.py
@@ -0,0 +1,18 @@
+print("Programming in Python")
+print('Programming in Python')
+print("""I know how to put "Quotes" into a string.\n\nAnd put in new lines!""")
+print("""I am using a multiline string to:
+-print multiple lines
+-without the use of the escape character \n""")
+
+
+str1 = "string 1"
+str2 = "string 2"
+
+# Concatenation version for 5.
+# str1 = "Hi, I am string 1 "
+# str2 = "and I am string 2!"
+# print(str1 + str2)
+
+print(f"Hi, I am {str1} and I am {str2}!")
+print(f"{str1} is before {str2} and {str2} is after {str1}")
---
+++ task 2/2.4.py
@@ -0,0 +1,8 @@
+
+num1 = input("Please enter a number:\n")
+num2 = input("Please enter another number:\n")
+
+num1(float)
+num2(float)
+
+print(f"The result of multiplying {num1} by {num2} is ({num1} * {num2})")
